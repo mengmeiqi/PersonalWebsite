be8e4af5e032b0534e3dc6efe1f473644ec3aab9
mengmeiqi <mengmeiqi0727@sina.com>
2018-01-31 21:29:15 +0800

    PersonalWebsite

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by dell on 2018/1/31.
+ */
+
+/*header里面的时间 start*/
+var oClock = document.getElementById("clock");
+var time = new Date();
+var now = time.toString();
+oClock.innerHTML = now;
+/*header里面的时间 end*/
+
