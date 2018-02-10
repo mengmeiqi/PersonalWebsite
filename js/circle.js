d8929b80bdb49e08571783e98b4f417ab2991eed
mengmeiqi <mengmeiqi0727@sina.com>
2018-02-10 19:20:47 +0800

    PersonalWebsite

---
+++ js/circle.js
@@ -0,0 +1,125 @@
+/*
+ *  Project: circle.js
+ *  Description: A library for focusing page elements.
+ *  Author: Greasidis Thodoris, Nona Maria
+ *  License: MIT
+ */
+
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ( $, window, document, undefined ) {
+
+    // undefined is used here as the undefined global variable in ECMAScript 3 is
+    // mutable (ie. it can be changed by someone else). undefined isn't really being
+    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+    // can no longer be modified.
+
+    // window and document are passed through as local variable rather than global
+    // as this (slightly) quickens the resolution process and can be more efficiently
+    // minified (especially when both are regularly referenced in your plugin).
+
+    // Create the defaults once
+    var pluginName = "circle",
+        defaults = {
+            animate: false,
+            animationMode: 'transition' // 'animation'
+        };
+
+    // The actual plugin constructor
+    function Plugin( element, options ) {
+        this.element = element;
+
+        // jQuery has an extend method which merges the contents of two or
+        // more objects, storing the result in the first object. The first object
+        // is generally empty as we don't want to alter the default options for
+        // future instances of the plugin
+        this.options = $.extend( {}, defaults, options );
+
+        this._defaults = defaults;
+        this._name = pluginName;
+
+        this.init();
+    }
+
+    Plugin.prototype = {
+
+        init: function() {
+            // Place initialization logic here
+            // You already have access to the DOM element and
+            // the options via the instance, e.g. this.element
+            // and this.options
+            // you can add more functions like the one below and
+            // call them like so: this.yourOtherFunction(this.element, this.options).
+
+            var $target = $(this.element);
+            var targetOffset = $target.offset();
+
+            var $circle = $('.CircleJS');
+            if (!$circle.length) {
+                var cssClass = "CircleJS";
+                if (this.options.animate) {
+                    if (this.options.animationMode === 'animation') {
+                        cssClass += " CircleJSAnimated";
+                    }
+                }
+
+                $circle = $('<div class="' + cssClass + '"></div>');
+                $circle.appendTo('body');
+            }
+
+            var h = $target.outerHeight();
+            var w = $target.outerWidth();
+            var diag = Math.sqrt(h*h + w*w);
+
+            // assuming that all border widths are equal
+            var border = parseFloat($circle.css('border-top-width'));
+            var paddingTop = parseFloat($circle.css('padding-top'));
+            var paddingLeft = parseFloat($circle.css('padding-left'));
+
+            $circle.css({
+                'top': Math.ceil(targetOffset.top + h/2 - diag/2 - border - paddingTop),
+                'left': Math.ceil(targetOffset.left + w/2 - diag/2 - border - paddingLeft),
+                'height': Math.ceil(diag),
+                'width': Math.ceil(diag)
+            });
+
+            if (this.options.animate && this.options.animationMode === 'transition') {
+                var initialScale = 5;
+
+                $circle.css({
+                    'transform': 'scale(' + initialScale + ')',
+                });
+
+                setTimeout(function(){
+                    $circle.addClass('CircleJSTransitioned');
+                    $circle.css({
+                        'transform': 'scale(1)',
+                        'opacity': 1
+                    });
+                }, 0);
+            }
+        },
+
+        destroy: function(el, options) {
+            var $circle = $('.CircleJS');
+            if ($circle && $circle.length) {
+                $circle.remove();
+            }
+        }
+    };
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            var instance = $.data(this, "plugin_" + pluginName);
+            if (!instance) {
+                instance = new Plugin( this, options );
+                $.data(this, "plugin_" + pluginName, instance);
+            } else if (options === 'destroy') {
+                instance.destroy();
+            }
+        });
+    };
+
+})( jQuery, window, document );
--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-/**
- * Created by dell on 2018/1/31.
- */
 
 /*header里面的时间 start*/
 var oClock = document.getElementById("clock");
@@ -39,59 +36,94 @@ window.onload = show;
 /*nav 图片下面的字符串 end*/
 
 /*section skill 进度条 start*/
-window.onload = function() {
-    var canvas = document.getElementById('canvas'),  //获取canvas元素
-        context = canvas.getContext('2d'),  //获取画图环境，指明为2d
-        centerX = canvas.width / 2,   //Canvas中心点x轴坐标
-        centerY = canvas.height / 2,  //Canvas中心点y轴坐标
-        rad = Math.PI * 2 / 100, //将360度分成100份，那么每一份就是rad度
-        speed = 0.1; //加载的快慢就靠它了
-
-    //绘制5像素宽的运动外圈
-    function blueCircle(n) {
-        context.save();
-        context.strokeStyle = "#337ab7"; //设置描边样式
-        context.lineWidth = 15; //设置线宽
-        context.beginPath(); //路径开始
-        context.arc(centerX, centerY, 60, -Math.PI / 2, -Math.PI / 2 + n * rad, false); //用于绘制圆弧context.arc(x坐标，y坐标，半径，起始角度，终止角度，顺时针/逆时针)
-        context.stroke(); //绘制
-        context.closePath(); //路径结束
-        context.restore();
+window.onload = function(){
+    function toCanvas(id ,progress){
+        var canvas = document.getElementById(id),
+            ctx = canvas.getContext("2d"),
+            percent = progress,
+            circleX = canvas.width / 2,
+            circleY = canvas.height / 2,
+            radius = 60,
+            lineWidth = 5,
+            fontSize = 20;
+        function smallcircle1(cx, cy, r) {
+            ctx.beginPath();
+                ctx.moveTo(cx + r, cy);
+            ctx.lineWidth = 1;
+            ctx.fillStyle = '#06a8f3';
+            ctx.arc(cx, cy, r,0,Math.PI*2);
+            ctx.fill();
+        }
+        function smallcircle2(cx, cy, r) {
+            ctx.beginPath();
+                ctx.moveTo(cx + r, cy);
+            ctx.lineWidth = 1;
+            ctx.fillStyle = '#00f8bb';
+            ctx.arc(cx, cy, r,0,Math.PI*2);
+            ctx.fill();
+        }
+        function circle(cx, cy, r) {
+            ctx.beginPath();
+            //ctx.moveTo(cx + r, cy);
+            ctx.lineWidth = lineWidth;
+            ctx.strokeStyle = '#eee';
+            ctx.arc(cx, cy, r, 0,Math.PI*2);
+            ctx.stroke();
+        }
+        function sector(cx, cy, r, startAngle, endAngle, anti) {
+            ctx.beginPath();
+            //ctx.moveTo(cx, cy + r); // 从圆形底部开始画
+            ctx.lineWidth = lineWidth;
+            // 渐变色 - 可自定义
+            var linGrad = ctx.createLinearGradient(
+                circleX-radius-lineWidth, circleY, circleX+radius+lineWidth, circleY
+            );
+            linGrad.addColorStop(0.0, '#06a8f3');
+            //linGrad.addColorStop(0.5, '#9bc4eb');
+            linGrad.addColorStop(1.0, '#00f8bb');
+            ctx.strokeStyle = linGrad;
+            ctx.lineCap = 'round';
+            ctx.arc(
+                cx, cy, r,
+                (Math.PI*2/3),
+                (Math.PI*2/3) + endAngle/100 * (Math.PI*5/3),
+                false
+            );
+            ctx.stroke();
+        }
+        function loading() {
+            if (process >= percent) {
+                clearInterval(circleLoading);
+            }
+            ctx.clearRect(0, 0, circleX * 2, circleY * 2);
+            ctx.font = fontSize + 'px April';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.fillStyle = '#999';
+            ctx.fillText(parseFloat(process).toFixed(0) + '%', circleX, circleY);
+            circle(circleX, circleY, radius);
+            sector(circleX, circleY, radius, Math.PI*2/3, process);
+            if (process / percent > 0.90) {
+                process += 0.30;
+            } else if (process / percent > 0.80) {
+                process += 0.55;
+            } else if (process / percent > 0.70) {
+                process += 0.75;
+            } else {
+                process += 1.0;
+            }
+        }
+        var process = 0.0;
+        var circleLoading = window.setInterval(function () {
+            loading();
+        }, 20);
     }
-
-    //绘制白色外圈
-    function whiteCircle() {
-        context.save();
-        context.beginPath();
-        context.lineWidth = 15; //设置线宽
-        context.strokeStyle = "#AAA";
-        context.arc(centerX, centerY, 60, 0, Math.PI * 2, false);
-        context.stroke();
-        context.closePath();
-        context.restore();
-    }
-
-    //百分比文字绘制
-    function text(n) {
-        context.save(); //save和restore可以保证样式属性只运用于该段canvas元素
-        context.strokeStyle = "#AAA"; //设置描边样式
-        context.font = "40px Arial"; //设置字体大小和字体
-        //绘制字体，并且指定位置
-        context.strokeText(n.toFixed(0) + "%", centerX - 30, centerY + 10);
-        context.stroke(); //执行绘制
-        context.restore();
-    }
-
-    //动画循环
-    (function drawFrame() {
-        window.requestAnimationFrame(drawFrame);
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        whiteCircle();
-        text(speed);
-        blueCircle(speed);
-        if (speed > 80) speed = 0;
-        speed += 0.1;
-    }());
+    toCanvas("circle-1",93);
+    toCanvas("circle-2",95);
+    toCanvas("circle-3",90);
+    toCanvas("circle-4",85);
+    toCanvas("circle-5",90);
+    toCanvas("circle-6",80);
 };
 /*section skill 进度条 end*/
 
